20b318f20a7baafd67f83f63b9aa88f37f3aea1e
chipiutyy25010304 <phuongkv1975@gmail.com>
2025-08-19 17:16:45 +0700

    Update functions.js

--- file/functions.js
+++ file/functions.js
@@ -55,6 +55,6 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result =  days + "ngày <span class=\"digit\">" + hours + "</span> giờ <span class=\"digit\">" + minutes + "</span> phút <span class=\"digit\">" + seconds + "</span> giây <span class=\"digit\">" + "</span> bên em"; 
 	$("#clock").html(result);
 }
